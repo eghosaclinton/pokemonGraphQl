72c9c32e0c601834184827673e8710d0d9f4bab0
clintt-09 <aisosaclinton09@gmail.com>
2024-10-16 12:52:51 +0100

    did some cleaning in fallback.tsx

--- src/components/FallBack.tsx
+++ src/components/FallBack.tsx
@@ -1,9 +1,6 @@
 import pokeLoader from '../assets/pokemon_121114.svg'
-import { useState } from 'react';
 
 export default function FallBack(){
-    const [count, setCount] = useState(0);
-    const arr = ['.', '..', '...', '....', '.....']
     // setInterval(()=>{
     //     setCount(prev=>{
     //         return prev >= arr.length ? 0 : (prev+1);      
@@ -15,7 +12,7 @@ export default function FallBack(){
             <div className='loader' >
                 <img src={pokeLoader} alt="Pokeball image loader" />
             </div>
-            <h3>Loading{setInterval(()=>{return 'n'}, 1500)}</h3>            
+            <h3>Loading...</h3>            
         </div>
     )
 }
