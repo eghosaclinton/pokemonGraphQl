ec437c4f2fa943ea96a15064698732b5bc9bf861
clintt-09 <aisosaclinton09@gmail.com>
2024-10-16 23:16:45 +0100

    initial commit for branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
 import './App.css'
 import pokeImg from './assets/pokeball.png'
-// import RootPage from './pages/RootPage'
-// import { Routes, Link, Route, BrowserRouter } from 'react-router-dom'
+import RootPage from './pages/RootPage'
+import { Routes, Link, Route, BrowserRouter } from 'react-router-dom'
 import PokemonContainer from './container/PokemonContainer'
 
 function App() {
 
   return (
    
-    <>
-      <header>
-        <h1 className='icon'>P<img src={pokeImg} alt="" />keCards</h1>
-      </header>
-      <main>
-        <PokemonContainer />
-      </main>
-    </>
-    //  <BrowserRouter>
-    //     <Routes>
-    //       <Route path='/' element={<RootPage />} />
-    //       <Route path='/pokemons' element={<PokemonContainer />}/>
-    //       <Route />
-    //       <Route />
-    //     </Routes>
-      
-    // </BrowserRouter>
+ 
+     <BrowserRouter>
+         <>
+        <header>
+          <h1 className='icon'>P<img src={pokeImg} alt="" />keCards</h1>
+        </header>
+        <main>
+          <PokemonContainer />
+          <Routes>
+            <Route path='/' element={<RootPage />} />
+            <Route path='/pokemons' element={<PokemonContainer />}/>
+            <Route />
+            <Route />
+          </Routes>
+        </main>
+      </>
+    </BrowserRouter>
   )
 }
 
