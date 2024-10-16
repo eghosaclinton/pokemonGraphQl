6e4f6b222700f662a7ac4c62a02d65287ec28315
clintt-09 <aisosaclinton09@gmail.com>
2024-10-16 23:11:27 +0100

    styling and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import './App.css'
+import pokeImg from './assets/pokeball.png'
+// import RootPage from './pages/RootPage'
+// import { Routes, Link, Route, BrowserRouter } from 'react-router-dom'
 import PokemonContainer from './container/PokemonContainer'
 
 function App() {
 
   return (
-    <main>
-      <PokemonContainer />
-    </main>
+   
+    <>
+      <header>
+        <h1 className='icon'>P<img src={pokeImg} alt="" />keCards</h1>
+      </header>
+      <main>
+        <PokemonContainer />
+      </main>
+    </>
+    //  <BrowserRouter>
+    //     <Routes>
+    //       <Route path='/' element={<RootPage />} />
+    //       <Route path='/pokemons' element={<PokemonContainer />}/>
+    //       <Route />
+    //       <Route />
+    //     </Routes>
+      
+    // </BrowserRouter>
   )
 }
 
